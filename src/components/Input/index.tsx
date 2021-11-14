7063c593677d1ee8b91270fe70d5e20d4517a970
luizsmatos <gusstavopdm@gmail.com>
2021-11-14 16:28:01 -0300

    refactor: components/Input, JavaScript para TypeScript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,13 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  icon?: React.ComponentType<{ size: number }>;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
